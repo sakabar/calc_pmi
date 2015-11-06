0ac85df9e0d4a828bbe9989ede3cde7e823f0db5
Takafumi Sakakibara <takafumi.sakakibara.pdx@gmail.com>
2015-11-06 13:47:26 +0900

    New: count_arg_of_pred

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # calc_pmi
 calculate PMI of a pred and a argument.
+
+
+格フレームのコーパスから、項の動詞のPMIを計算する
+データは秘密。
+
+src/count_arg_of_pred.py : ある動詞について、その動詞とある項が共起する回数が収められたファイル(/path/to/pa.data.basic.split/web15G-201306-237739.data.basicなど)を標準入力から読み込み、その動詞の回数の和と(1行目)と、項ごとの回数を出力する
+
+`lv /path/to/pa.data.basic.split/web15G-201306-237739.data.basic | python src/count_arg_of_pred.py > web15G-201306-237739.data.basic.count`
+
+shell/count_pred_of_arg.sh : src/count_arg_of_pred.pyを、forループで回す
+
+shell/output_case.sh : 生の文字列を標準入力から受け取り、格の情報を出力する
---
+++ shell/count_pred_of_arg.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+set -ue
+
+#$input_dirに置いてあるファイルそれぞれに対してsrc/count_arg_of_pred.pyを実行し、
+#結果を$output_dir内に格納する
+
+if [ `hostname` != 'tsumire' ]; then
+    echo "run in server 'tsumire'">&2 #tsumireにデータが置いてある。ここ今はベタ打ち。
+    exit 1
+fi
+
+input_dir='/local2/sasano/pa.data.basic.split'
+output_dir='/local/tsakaki/pa.data.basic.split.count'
+
+file_cnt=0
+for f in $input_dir/*.basic; do
+# for f in `ls $input_dir | tail -n 100`; do
+    file_cnt=$(( file_cnt + 1 ))
+    echo "$file_cnt / 261667"
+    # echo "$file_cnt / 100"
+    lv $input_dir/$f:t | python src/count_arg_of_pred.py > $output_dir/$f:t
+done
